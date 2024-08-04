2495027e1d5cc09342b363b37d498f220f6f4eb6
sorry <247076126@qq.com>
2024-08-04 21:28:11 +0800

    1

--- src/app.jsx
+++ src/app.jsx
@@ -1,26 +1,6 @@
 import { createSignal, onMount } from 'solid-js'
 import { builder } from "../flow/analysis";
-// import { sandBox, __codeRecodeScope__ } from "../flow/core";
-
-// const code = builder(`
-// function sumOfOdds(n) {
-//     let sum = 0;
-//     for (let i = 1; i <= n; i++) {
-
-//       if(i === 5){
-//         console.log(5);
-//       }else{
-//         console.log(6);
-//       }
-//     }
-//     return sum;
-//   }
-//  sumOfOdds(10)
-//   `);
-// console.log(code);
-
-// console.log(sandBox(code).__codeRecodeScope__
-//   .currentScope);
+import { sandBox, } from "../flow/core";
 
 
 function App() {
@@ -37,8 +17,14 @@ function App() {
 }
 sumOfOdds(10);`)
   const [translateCode, setTranslateCode] = createSignal("");
+  const [recode, setRecode] = createSignal(null)
   function translate() {
     setTranslateCode(builder(code()))
+    const r = sandBox(translateCode());
+    setRecode(r.__codeRecodeScope__
+      .currentScope)
+    console.log(r.__codeRecodeScope__
+      .currentScope);
   }
   onMount(translate)
 
